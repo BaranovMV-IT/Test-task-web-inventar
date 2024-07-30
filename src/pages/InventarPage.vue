e92f10307d66f41bad5398aff2eb792ca4eab9e3
BaranovMV-IT <mishasamarabaranov2002@mail.ru>
2024-07-30 19:30:29 +0400

    Base setup

---
+++ src/pages/InventarPage.vue
@@ -0,0 +1,17 @@
+<template>
+  
+</template>
+
+<script setup lang="ts">
+import { defineComponent, ref } from 'vue';
+
+defineComponent({
+  name: 'InventarPage',
+});
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
