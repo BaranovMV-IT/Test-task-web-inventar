7002ffa62c1b76ee331a1407f831dac4a55948b2
BaranovMV-IT <mishasamarabaranov2002@mail.ru>
2024-07-31 16:05:30 +0400

    Formatting the code

--- src/pages/InventarPage.vue
+++ src/pages/InventarPage.vue
@@ -16,19 +16,33 @@
         </div>
       </div>
       <div class="page__main__inventor">
-        <div class="page__main__inventor__cell" v-for="cell in inventar" :key="cell.id_in" :id="'cell'+cell.id_in" :style="{backgroundColor: cell.color}">
-          <div class="page__main__inventor__cell__item" draggable="true" v-if="cell.item.id_it" @click="openModal(cell.item)" @dragstart="e=>dragStart(e)" @drag="e=>drag(e, cell.item.id_it)" @dragend="e=>dragEnd(e, cell.item, cell.id_in)">
+        <div class="page__main__inventor__cell" 
+          v-for="cell in inventar" 
+          :key="cell.id_in" 
+          :id="'cell'+cell.id_in" 
+          :style="{backgroundColor: cell.color}">
+          <div class="page__main__inventor__cell__item" 
+            draggable="true" 
+            v-if="cell.item.id_it" 
+            @click="openModal(cell.item)" 
+            @dragstart="e=>dragStart(e)" 
+            @drag="e=>drag(e, cell.item.id_it)" 
+            @dragend="e=>dragEnd(e, cell.item, cell.id_in)">
             <div class="page__main__inventor__cell__item__placeholder">
-              <div class="page__main__inventor__cell__item__placeholder__square" :style="{left: 0, bottom: 0, background: `linear-gradient(45deg, rgb(${cell.item.color?.split(',').map(n=>Number(n)*0.25).join(',')}) 0%, rgb(${cell.item.color}) 100%)`}"></div>
-              <div class="page__main__inventor__cell__item__placeholder__square" :style="{right: 0, top: 0, zIndex: 2, background: `linear-gradient(45deg, rgb(${cell.item.color?.split(',').map(n=>Number(n)*0.25).join(',')}) 0%, rgba(${cell.item.color},0.85) 100%)`}"></div>
+              <div class="page__main__inventor__cell__item__placeholder__square" 
+                :style="{left: 0, bottom: 0, background: `linear-gradient(45deg, rgb(${cell.item.color?.split(',').map(n=>Number(n)*0.25).join(',')}) 0%, rgb(${cell.item.color}) 100%)`}"></div>
+              <div class="page__main__inventor__cell__item__placeholder__square" 
+                :style="{right: 0, top: 0, zIndex: 2, background: `linear-gradient(45deg, rgb(${cell.item.color?.split(',').map(n=>Number(n)*0.25).join(',')}) 0%, rgba(${cell.item.color},0.85) 100%)`}"></div>
             </div>
             <div class="page__main__inventor__cell__item__count">{{ cell.item.count }}</div>
           </div>
         </div>
         <div class="page__main__inventor__modal" v-if="isModalOpen">
           <div class="page__main__inventor__cell__item__placeholder  modal-placeholder">
-            <div class="page__main__inventor__cell__item__placeholder__square" :style="{left: 0, bottom: 0, background: `linear-gradient(45deg, rgb(${selectedItem.color?.split(',').map(n=>Number(n)*0.25).join(',')}) 0%, rgb(${selectedItem.color}) 100%)`}"></div>
-            <div class="page__main__inventor__cell__item__placeholder__square" :style="{right: 0, top: 0, zIndex: 2, background: `linear-gradient(45deg, rgb(${selectedItem.color?.split(',').map(n=>Number(n)*0.25).join(',')}) 0%, rgba(${selectedItem.color},0.85) 100%)`}"></div>
+            <div class="page__main__inventor__cell__item__placeholder__square" 
+              :style="{left: 0, bottom: 0, background: `linear-gradient(45deg, rgb(${selectedItem.color?.split(',').map(n=>Number(n)*0.25).join(',')}) 0%, rgb(${selectedItem.color}) 100%)`}"></div>
+            <div class="page__main__inventor__cell__item__placeholder__square" 
+              :style="{right: 0, top: 0, zIndex: 2, background: `linear-gradient(45deg, rgb(${selectedItem.color?.split(',').map(n=>Number(n)*0.25).join(',')}) 0%, rgba(${selectedItem.color},0.85) 100%)`}"></div>
           </div>
           <div class="divider"></div>
           <div class="page__main__inventor__modal__info">
@@ -40,12 +54,28 @@
             <div class="skeleton" style="width: 33%; height: 7.5%; margin: 0 0 5% 0;"></div>
           </div>
           <div class="divider"></div>
-          <button class="page__main__inventor__modal__remove-button" @click="openModalFooter()">Удалить предмет</button>
+          <button class="page__main__inventor__modal__remove-button" 
+            @click="openModalFooter()"
+          >
+            Удалить предмет
+          </button>
           <div class="page__main__inventor__modal__change-menu" v-if="isModalFooterOpen">
-            <input v-model="inputValue" type="number" class="page__main__inventor__modal__change-menu__input" placeholder="Введите количество">
+            <input class="page__main__inventor__modal__change-menu__input"
+              v-model="inputValue" 
+              type="number" 
+              placeholder="Введите количество"
+            >
             <div class="page__main__inventor__modal__change-menu__footer">
-              <button class="page__main__inventor__modal__change-menu__footer__cancel-button  footer-buttons" @click="closeModalFooter()">Отмена</button>
-              <button class="page__main__inventor__modal__change-menu__footer__apply-button  footer-buttons" @click="changeItemCount()">Подтвердить</button>
+              <button class="page__main__inventor__modal__change-menu__footer__cancel-button  footer-buttons" 
+                @click="closeModalFooter()"
+              >
+                Отмена
+              </button>
+              <button class="page__main__inventor__modal__change-menu__footer__apply-button  footer-buttons" 
+                @click="changeItemCount()"
+              >
+                Подтвердить
+              </button>
             </div>
           </div>
           <button class="close-button" @click="closeModal()"></button>
@@ -248,29 +278,38 @@ onMounted(() => {
 </script>
 
 <style lang="scss" scoped>
+$pagePadding: calc(100vw / 38.4);
+$blockMargin: calc(100vw / 54.85714);
+$borderWidth: calc(100vw / 960);
+$borderRadius: calc(100vw / 76.8);
+$blockPadding: calc(100vw / 46.8);
+$borderColor: rgb(77,77,77);
+$secondBackgroundColor: rgb(38,38,38);
+$textColor: rgb(125,125,125);
+
 .page {
   display: flex;
   flex-grow: 1;
   flex-direction: column;
-  padding: calc(100vw / 38.4);
+  padding: $pagePadding;
   &__main {
     display: flex;
     flex-grow: 1;
-    margin-bottom: calc(100vw / 54.85714);
+    margin-bottom: $blockMargin;
     &__aside {
       display: flex;
       flex-direction: column;
       width: 30%;
-      margin-right: calc(100vw / 54.85714);
-      border: calc(100vw / 960) solid rgb(77,77,77);
-      border-radius: calc(100vw / 76.8);
-      background-color: rgb(38,38,38);
-      padding: calc(100vw / 76.8);
+      margin-right: $blockMargin;
+      border: $borderWidth solid $borderColor;
+      border-radius: $borderRadius;
+      background-color: $secondBackgroundColor;
+      padding: $borderRadius;
       &__image {
         display: flex;
         justify-content: center;
         align-items: center;
-        border-radius: calc(100vw / 76.8);
+        border-radius: $borderRadius;
         overflow: hidden;
         aspect-ratio: 1/1.25;
         &__item {
@@ -291,17 +330,17 @@ onMounted(() => {
       display: flex;
       flex-grow: 1;
       flex-wrap: wrap;
-      border-radius: calc(100vw / 76.8);
-      background-color: rgb(38,38,38);
+      border-radius: $borderRadius;
+      background-color: $secondBackgroundColor;
       aspect-ratio: 1.05/1;
-      border-left: calc(100vw / 960) solid rgb(77,77,77);
-      border-top: calc(100vw / 960) solid rgb(77,77,77);
+      border-left: $borderWidth solid $borderColor;
+      border-top: $borderWidth solid $borderColor;
       overflow: hidden;
       &__cell {
         width: 20%;
         height: 20%;
-        border-right: calc(100vw / 960) solid rgb(77,77,77);
-        border-bottom: calc(100vw / 960) solid rgb(77,77,77);
+        border-right: $borderWidth solid $borderColor;
+        border-bottom: $borderWidth solid $borderColor;
         &__item {
           position: relative;
           width: 100%;
@@ -333,12 +372,12 @@ onMounted(() => {
             bottom: 0;
             height: 16%;
             min-width: 16%;
-            border-left: calc(100vw / 960) solid rgb(77,77,77);
-            border-top: calc(100vw / 960) solid rgb(77,77,77);
+            border-left: $borderWidth solid $borderColor;
+            border-top: $borderWidth solid $borderColor;
             border-top-left-radius: calc(100vw / 128);
-            color: rgb(125,125,125);
-            font-size: calc(100vw / 76.8);
-            background-color: rgb(38,38,38);
+            color: $textColor;
+            font-size: $borderRadius;
+            background-color: $secondBackgroundColor;
           }
         }
       }
@@ -349,11 +388,11 @@ onMounted(() => {
         align-items: center;
         width: 52%;
         top: 0;
-        bottom: calc(100vw / 960);
-        right: calc(100vw / 960);
-        padding: calc(100vw / 76.8);
-        background-color: rgb(38,38,38);
-        border-left: calc(100vw / 960) solid rgb(77,77,77);
+        bottom: $borderWidth;
+        right: $borderWidth;
+        padding: $borderRadius;
+        background-color: $secondBackgroundColor;
+        border-left: $borderWidth solid $borderColor;
         z-index: 2;
         .modal-placeholder {
           margin: 20% 0 15% 0;
@@ -370,7 +409,7 @@ onMounted(() => {
           background-color: transparent;
           width: 100%;
           padding: calc(100vw / 66.8) 0;
-          font-size: calc(100vw / 76.8);
+          font-size: $borderRadius;
           color: rgb(255, 255, 255);
           background-color: rgb(250,114,114);
           border-radius: calc(100vw / 86.8);
@@ -387,20 +426,20 @@ onMounted(() => {
           flex-direction: column;
           width: 100%;
           bottom: 0;
-          padding: calc(100vw / 46.8);
-          background-color: rgb(38,38,38);
-          border-top: calc(100vw / 960) solid rgb(77,77,77);
+          padding: $blockPadding;
+          background-color: $secondBackgroundColor;
+          border-top: $borderWidth solid $borderColor;
           z-index: 3;
           &__input {
             border: none;
             background-color: transparent;
             height: calc(100vw / 21.225);
-            border: calc(100vw / 960) solid rgb(77,77,77);
+            border: $borderWidth solid $borderColor;
             border-radius: calc(100vw / 206.8);
-            margin-bottom: calc(100vw / 46.8);
-            color: rgb(125,125,125);
+            margin-bottom: $blockPadding;
+            color: $textColor;
             font-size: calc(100vw / 56.8);
-            padding: 0 calc(100vw / 46.8) 0 calc(100vw / 66.8);
+            padding: 0 $blockPadding 0 calc(100vw / 66.8);
             &:focus {
               outline: none;
             }
@@ -417,8 +456,8 @@ onMounted(() => {
             &__cancel-button {
               color: rgb(45,45,45);
               background-color: rgb(255, 255, 255);
-              padding: 0 calc(100vw / 46.8);
-              margin-right: calc(100vw / 76.8);
+              padding: 0 $blockPadding;
+              margin-right: $borderRadius;
               box-shadow: 0 0 calc(100vw / 85) rgb(255, 255, 255);
             }
             &__apply-button {
@@ -439,10 +478,10 @@ onMounted(() => {
   &__info {
     position: relative;
     height: calc(100vw / 11.7917);
-    border: calc(100vw / 960) solid rgb(77,77,77);
-    border-radius: calc(100vw / 76.8);
-    background-color: rgb(38,38,38);
-    padding: calc(100vw / 46.8);
+    border: $borderWidth solid $borderColor;
+    border-radius: $borderRadius;
+    background-color: $secondBackgroundColor;
+    padding: $blockPadding;
 
   }
 }
@@ -469,13 +508,13 @@ onMounted(() => {
 .divider {
   width: 100%;
   height: 0px;
-  border: calc(100vw / 1920) solid rgb(77,77,77);
+  border: calc(100vw / 1920) solid $borderColor;
 }
 
 .skeleton {
   width: 0;
   height: 0;
   border-radius: calc(100vw / 96.8);
-  background: linear-gradient(90deg, rgba(52,52,52,1) 0%, rgba(77,77,77,1) 33%, rgba(52,52,52,1) 100%);
+  background: linear-gradient(90deg, rgba(52,52,52,1) 0%, rgba($borderColor,1) 33%, rgba(52,52,52,1) 100%);
 }
 </style>
